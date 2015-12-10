5dfddfee7606dcca54d906c54b9168dcebf2bb90
qingfengwuyou <h3658686@126.com>
2015-12-10 19:48:57 +0800

    no message

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,3 +3,50 @@
 module.exports.index = function(req, res, next) {
   res.render('index', { title: '首页' });
 };
+
+module.exports.indexhby = function(req, res, next) {
+  res.render('index-hby', { title: '首页hby' });
+};
+
+module.exports.indexlhg = function(req, res, next) {
+  res.render('index-lhg', { title: '首页lhg' });
+};
+
+module.exports.indexzfh = function(req, res, next) {
+  res.render('index-zfh', { title: '首页zfh' });
+};
+
+module.exports.indexczp = function(req, res, next) {
+  res.render('index-czp', { title: '首页czp' });
+};
+
+module.exports.invoicehby = function(req, res, next) {
+  res.render('invoice', { title: 'invoice hby' });
+};
+
+module.exports.profilelhg = function(req, res, next) {
+  res.render('profile', { title: 'profile lhg' });
+};
+
+module.exports.formgeneralzfh = function(req, res, next) {
+  res.render('form-general', { title: 'form-general zfh' });
+};
+module.exports.formadvancedzfh = function(req, res, next) {
+  res.render('form-advanced', { title: 'form-advanced zfh' });
+};
+module.exports.tablesimplezfh = function(req, res, next) {
+  res.render('table-simple', { title: 'tablesimple zfh' });
+};
+module.exports.tabledataczp = function(req, res, next) {
+  res.render('table-data', { title: 'table-data czp' });
+};
+
+module.exports.mail1czp = function(req, res, next) {
+  res.render('mail1', { title: 'mail1 czp' });
+};
+module.exports.mail2czp = function(req, res, next) {
+  res.render('mail2', { title: 'mail2 czp' });
+};
+module.exports.mail3czp = function(req, res, next) {
+  res.render('mail3', { title: 'mail3 czp' });
+};
--- server/routes/index.js
+++ server/routes/index.js
@@ -6,5 +6,20 @@ var indexController = require('../controllers/index');
 
 /* GET home page. */
 router.get('/', indexController.index);
+router.get('/index-hby', indexController.indexhby);
+router.get('/index-lhg', indexController.indexlhg);
+router.get('/index-zfh', indexController.indexzfh);
+router.get('/index-czp', indexController.indexczp);
+
+router.get('/invoice', indexController.invoicehby);
+router.get('/profile', indexController.profilelhg);
+router.get('/form-general', indexController.formgeneralzfh);
+router.get('/form-advanced', indexController.formadvancedzfh);
+router.get('/table-simple', indexController.tablesimplezfh);
+router.get('/table-data', indexController.tabledataczp);
+router.get('/mail1', indexController.mail1czp);
+router.get('/mail2', indexController.mail2czp);
+router.get('/mail3', indexController.mail3czp);
+
 
 module.exports = router;
