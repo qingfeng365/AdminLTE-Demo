80690f3b10f4aefd2c28a0564526e0e521059426
happychen998 <happychen998@hotmail.com>
2015-12-14 21:36:47 +0800

    完成数据表格

--- client/js/dashboard_indexdemo.js
+++ client/js/dashboard_indexdemo.js
@@ -272,3 +272,16 @@ $(function () {
     });
   });
 });
+
+//add by czp
+$(function () {
+  $("#example1").DataTable();
+  $('#example2').DataTable({
+    "paging": true,
+    "lengthChange": false,
+    "searching": false,
+    "ordering": true,
+    "info": true,
+    "autoWidth": false
+  });
+});
